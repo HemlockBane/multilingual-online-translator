f75a4c5c7349c9cf8ce8eac595f2998d962a9c57
Obinna Igwe <igwenus619@gmail.com>
2020-11-20 23:04:43 +0100

    Add support for more languages

--- main.py
+++ main.py
@@ -3,22 +3,39 @@ from bs4 import BeautifulSoup
 
 lang_codes = {'en': 'english', 'fr': 'french'}
 
-target_lang = input(
-    'Type "en" if you want to translate from French into English, or "fr" if you want to translate from English into French:')
-text = input('Type the word you want to translate:')
-print(f'You chose "{target_lang}" as the language to translate "{text}".')
+supported_langs = ['Arabic',
+                   'German',
+                   'English',
+                   'Spanish',
+                   'French',
+                   'Hebrew',
+                   'Japanese',
+                   'Dutch',
+                   'Polish',
+                   'Portuguese',
+                   'Romanian',
+                   'Russian',
+                   'Turkish']
 
-src_lang = 'en'
-if target_lang == 'en':
-    src_lang = 'fr'
+print("Hello, you're welcome to the translator. Translator supports: ")
+for idx, lang in enumerate(supported_langs, 1):
+    print(f'{idx}. {lang}')
+src_lang_idx = int(input('Type the number of your language: '))
+target_lang_idx = int(input('Type the number of language you want to translate to: '))
+text = input('Type the word you want to translate:')
+
+src_lang = supported_langs[src_lang_idx - 1]
+target_lang = supported_langs[target_lang_idx - 1]
 
 base_translation_url = 'https://context.reverso.net/translation/'
-translation_direction = f'{lang_codes[src_lang]}-{lang_codes[target_lang]}/'
+translation_direction = f'{src_lang.lower()}-{target_lang.lower()}/'
 
 url = f'{base_translation_url}{translation_direction}{text}'
 chrome_user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
 headers = {
     'user-agent': chrome_user_agent}
+
+# print(url)
 
 res = requests.get(url, headers=headers)
 print(f'{res.status_code} {res.reason}', end='\n\n')
@@ -29,7 +46,7 @@ filtered_contents = [i for i in div_tag.contents if i not in ['\n']]
 translations = [i.text.strip() for i in filtered_contents]
 
 print('Context examples:', end='\n\n')
-print('French Translations:')
+print(f'{target_lang} Translations:')
 for v in translations[:5]:
     print(v)
 print('')
@@ -45,7 +62,7 @@ for v in filtered_contents:
     if trg:
         examples.append(trg.text)
 filtered_examples = [i.strip() for i in examples if i not in ['\n']]
-print('French Examples:', sep='\n')
+print(f'{target_lang} Examples:', sep='\n')
 # print(filtered_examples)
 for i, v in enumerate(filtered_examples[:10], 1):
     print(v)
